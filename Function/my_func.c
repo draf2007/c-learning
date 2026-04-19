4ac47d6b8da94a5a2e5d4732b7f5f0775818b048
draf2007 <draf2007@gmail.com>
2026-04-19 11:23:44 +0300

    add func

--- Function/my_func.c
+++ Function/my_func.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdarg.h>
 
 /*Декларация (объявление) функции*/
 void say_hello() {
@@ -11,4 +12,30 @@ void hello_name(const char* name) {
 
 void hello_name_age(const char* name, int age) {
     printf("Hello, %s - %d\n",name,age);
+}
+
+/*void average_m(int  m_size, int m[]) {
+    long long summa = 0;
+    for (int i = 0; i < m_size; ++i)
+    {
+        summa += m[i];
+        
+    }
+    return (double)summa / m_size;
+}*/
+
+double average_n(int n,...) {
+    long long summa = 0;
+
+    va_list params;
+    va_start(params,n);
+
+    for (int i = 0; i < n; i++)
+    {
+        int k = va_arg(params,int);
+        summa +=k;
+    }
+    va_end(params);
+
+    return (double)summa / n;
 }
--- Function/my_func.h
+++ Function/my_func.h
@@ -3,4 +3,6 @@
 void say_hello();
 void hello_name(const char* name);
 void hello_name_age(const char* name, int age);
+void average_m(int m_size, int m[]);
+double average_n(int n,...)
 #endif
