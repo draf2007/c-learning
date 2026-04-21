a7193b13575df4767ada760a4efc6922619c44ec
draf2007 <draf2007@gmail.com>
2026-04-21 17:36:59 +0300

    swap in process

--- Test files&Trash/reference.c
+++ Test files&Trash/reference.c
@@ -1,13 +1,16 @@
-
 #include <stdio.h>
 #include <stdlib.h>
 
+int swap(int *ptr1, int *ptr2) {
+    int temp = *ptr1;
+    *ptr1 = *ptr2;
+    (*ptr2)=temp;
+    
+    return *ptr1, *ptr2; 
+}
+
 int main(int argc, char *argv[]) {
-    int a,b;
-    a=10;
-    b=15;
-    int *ptr;
-    ptr = &a;
-    printf("addr ptr = %p value ptr = %d",ptr,*ptr);
+    int x=5,y=10;
+    printf("%d\n %d\n",swap(&x,&y));
     return 0;
 }
